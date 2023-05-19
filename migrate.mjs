891702653a2cfd92e33ed614aa14f4b80d5630d8
sigmasoldi3r <pablobc.1995@gmail.com>
2023-05-19 21:42:33 +0200

    Migrated to YAML for my mental health's sake

--- migrate.mjs
+++ migrate.mjs
@@ -1,14 +1,20 @@
 import YAML from 'yaml';
 import JSON5 from 'json5';
 import { globIterateSync } from 'glob';
-import { readFileSync, writeFileSync } from 'fs'
+import { mkdirSync, readFileSync, writeFileSync } from 'fs'
 import { join, parse } from 'path';
 
-for (const file of globIterateSync('src/**/*.json5')) {
+for (const file of globIterateSync('old-src/**/*.json5')) {
   const content = readFileSync(file).toString();
   const data = JSON5.parse(content);
   const out = YAML.stringify(data);
   const filePath = parse(file);
-  const outPath = join(filePath.dir, filePath.name + '.yml');
+  const outDir = filePath.dir.replace(/^old-/, '')
+  const outPath = join(outDir, filePath.name + '.yml');
+  try {
+    mkdirSync(outDir, {
+      recursive: true,
+    })
+  } catch { }
   writeFileSync(outPath, out)
 }
